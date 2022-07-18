eccdab81a3ac37f1a302160953c27f5c91125f0c
naoki <naokipgt@gmail.com>
2022-07-18 10:49:40 +0900

    setup　スクリプト

--- regolith/setup.sh
+++ regolith/setup.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
+unlink ~/.config/regolith
+rm -r ~/.config/regolith
+ln -s `pwd`/../regolith ~/.config/regolith
 
+unlink ~/.config/polybar
+rm -r ~/.config/polybar
+ln -s `pwd`/polybar ~/.config/polybar
+
+sudo unlink /etc/regolith/styles/onedark
+sudo ln -s `pwd`/styles/onedark /etc/regolith/styles/onedark
